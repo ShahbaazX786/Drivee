5b3522ffff11ace4576dc6a1d5535c33a50de2c9
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-21 20:16:27 +0530

    Added 3 new components for onboarding screens

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView className="flex h-full justify-between items-center bg-white">
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
