4b9f94e46d383e9fd92b9bbb62f5bbc0e1bd2ef3
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-21 17:17:08 +0530

    Testing 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className='text-8xl'>Bismillah</Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
