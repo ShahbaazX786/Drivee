fc927b665e5b9634b16255de5bdad86ced06f166
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-22 13:11:57 +0530

    created sign in component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,61 @@
 /* eslint-disable prettier/prettier */
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "@/components/customButton";
+import InputField from "@/components/inputField";
+import OAuth from "@/components/oAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const logInUser = async () => {};
+
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-64">
+          <Image source={images.signUpCar} className="z-0 w-full h-64" />
+          <Text className="text-2xl font-JakartaSemiBold text-black absolute bottom-5 left-5">
+            Welcome Back 👋
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter your email address"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value: string) => {
+              setForm({ ...form, email: value });
+            }}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter your password"
+            icon={icons.lock}
+            value={form.password}
+            onChangeText={(value: string) => {
+              setForm({ ...form, password: value });
+            }}
+          />
+          <CustomButton title="Log In" onPress={logInUser} className={"mt-6"} />
+
+          <OAuth />
+          <Link
+            href={"/sign-up"}
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Don't have an Account? </Text>
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
