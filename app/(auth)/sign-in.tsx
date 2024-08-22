5f539813786d7c66d4c0b35772b0384aedfe7574
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-22 18:30:59 +0530

    followed docs and added sign in functionality

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -3,17 +3,40 @@ import CustomButton from "@/components/customButton";
 import InputField from "@/components/inputField";
 import OAuth from "@/components/oAuth";
 import { icons, images } from "@/constants";
-import { Link } from "expo-router";
-import { useState } from "react";
+import { useSignIn } from "@clerk/clerk-expo";
+import { Link, useRouter } from "expo-router";
+import { useCallback, useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 
 const SignIn = () => {
+  const router = useRouter();
+  const { signIn, setActive, isLoaded } = useSignIn();
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
-  const logInUser = async () => {};
+  const logInUser = useCallback(async () => {
+    if (!isLoaded) {
+      return;
+    }
+
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: form.email,
+        password: form.password,
+      });
+
+      if (signInAttempt.status === "complete") {
+        await setActive({ session: signInAttempt.createdSessionId });
+        router.replace("/");
+      } else {
+        console.error(JSON.stringify(signInAttempt, null, 2));
+      }
+    } catch (err: any) {
+      console.error(JSON.stringify(err, null, 2));
+    }
+  }, [isLoaded, form.email, form.password]);
 
   return (
     <ScrollView className="flex-1 bg-white">
