fd0037c756f216a7bfb323e19d1c94abf21a2625
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-22 16:24:38 +0530

    Updated root redirection and also added clerk auth checker hook.

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
 const MainPage = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href={"/(root)/(tabs)/home"} />;
+  }
   // just redirect to welcome page in auth layout
   return <Redirect href="/(auth)/welcome" />;
 };
