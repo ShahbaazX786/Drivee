ab1d2d0a5c3845ea05750ab964c35ffa9a9fefeb
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-23 18:25:39 +0530

    Added zustand store

---
+++ store/index.ts
@@ -0,0 +1,43 @@
+/* eslint-disable prettier/prettier */
+
+import { LocationStore } from "@/types/type";
+import { create } from "zustand";
+
+export const useLocationStore = create<LocationStore>((set) => ({
+  userAddress: null,
+  userLongitude: null,
+  userLatitude: null,
+  destinationAddress: null,
+  destinationLongitude: null,
+  destinationLatitude: null,
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      userLatitude: latitude,
+      userLongitude: longitude,
+      userAddress: address,
+    }));
+  },
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+    }));
+  },
+}));
