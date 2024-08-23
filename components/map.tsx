6f1ad355a5d6eaa5f9fdcb84cafc08348ee6cfad
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-23 18:42:54 +0530

    Added and used utility functions and enabled region in map view.

--- components/map.tsx
+++ components/map.tsx
@@ -1,8 +1,24 @@
 /* eslint-disable prettier/prettier */
 
+import { calculateRegion } from "@/lib/map";
+import { useLocationStore } from "@/store";
 import { Text } from "react-native";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 const Map = () => {
+  const {
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
+
   return (
     <MapView
       provider={PROVIDER_DEFAULT}
@@ -10,6 +26,7 @@ const Map = () => {
       tintColor="black"
       mapType="mutedStandard"
       showsPointsOfInterest={false}
+      initialRegion={region}
       showsUserLocation={true}
       userInterfaceStyle={"light"}
     >
