806bfd6d25a3ca7851c2107df83703e9b0d9211d
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-23 18:13:16 +0530

    Integrated map lib with map component.

--- components/map.tsx
+++ components/map.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+/* eslint-disable prettier/prettier */
 
+import { Text } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 const Map = () => {
   return (
-    <View>
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-full"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      showsUserLocation={true}
+      userInterfaceStyle={"light"}
+    >
       <Text>Map</Text>
-    </View>
+    </MapView>
   );
 };
 
