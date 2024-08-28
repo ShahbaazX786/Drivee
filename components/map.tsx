607f8cd882f102eaca06c3a0f1685e05fd8dfb19
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-08-28 18:37:49 +0530

    Added mock drivers and visualizing markers in map.

--- components/map.tsx
+++ components/map.tsx
@@ -1,9 +1,12 @@
 /* eslint-disable prettier/prettier */
 
-import { calculateRegion } from "@/lib/map";
-import { useLocationStore } from "@/store";
-import { Text } from "react-native";
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { icons } from "@/constants";
+import { mock_drivers } from "@/constants/constants";
+import { calculateRegion, generateMarkersFromData } from "@/lib/map";
+import { useDriverStore, useLocationStore } from "@/store";
+import { MarkerData } from "@/types/type";
+import { useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
 const Map = () => {
   const {
     userLatitude,
@@ -11,13 +14,27 @@ const Map = () => {
     destinationLatitude,
     destinationLongitude,
   } = useLocationStore();
-
+  const { selectedDriver, setDrivers, setSelectedDriver } = useDriverStore();
+  const [markers, setMarkers] = useState<MarkerData[]>([]);
   const region = calculateRegion({
     userLatitude,
     userLongitude,
     destinationLatitude,
     destinationLongitude,
   });
+
+  useEffect(() => {
+    const drivers = mock_drivers;
+    if (Array.isArray(drivers)) {
+      if (!userLatitude || !userLongitude) return;
+      const newMarkers = generateMarkersFromData({
+        data: drivers,
+        userLatitude,
+        userLongitude,
+      });
+      setMarkers(newMarkers);
+    }
+  }, [userLatitude, userLongitude]);
 
   return (
     <MapView
@@ -30,7 +47,19 @@ const Map = () => {
       showsUserLocation={true}
       userInterfaceStyle={"light"}
     >
-      <Text>Map</Text>
+      {markers.map((marker) => (
+        <Marker
+          key={marker.id}
+          coordinate={{
+            latitude: marker.latitude,
+            longitude: marker.longitude,
+          }}
+          title={marker.title}
+          image={
+            selectedDriver === marker.id ? icons.selectedMarker : icons.marker
+          }
+        />
+      ))}
     </MapView>
   );
 };
